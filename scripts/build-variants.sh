c0d5755de3310b5819279844a241bb649c878058
Phong Nguyen <nhphong1406@gmail.com>
2021-02-08 22:42:01 +0700

    Add build all scripts

---
+++ scripts/build-variants.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+./scripts/build-premade.sh
+./scripts/build-premade-375.sh
+
+./scripts/build-phong.sh
+./scripts/build-phong-375.sh
+
+./scripts/build-default.sh
+./scripts/build-default-375.sh
